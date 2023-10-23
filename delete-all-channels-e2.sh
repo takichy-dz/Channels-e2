6e00c60f192da279206cd3a89ea495d8761e812f
takichy-dz <75120902+takichy-dz@users.noreply.github.com>
2023-10-23 18:35:57 +0100

    Create delete-all-channels-e2.sh

---
+++ delete-all-channels-e2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+init 4
+echo "############ Delete old Channels #############"
+PATH1='/etc/enigma2'
+cd $PATH1
+rm -rf *.tv lame* bouq* user* black* white*
+sleep 1
+echo           "###### Script by Takichy *** enjoy *** ###########"
+
+exit 0
